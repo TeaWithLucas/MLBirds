f2506d8bca580e4255b51f9e0feb66000b397e8e
TeaWithLucas <18538046+TeaWithLucas@users.noreply.github.com>
2021-05-20 14:59:33 +0100

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -17,17 +17,17 @@ part_click_locs_file = analysis_fldr + 'parts/part_click_locs.csv'
 part_locs_file = analysis_fldr + 'parts/part_locs.csv'
 parts_file = analysis_fldr + 'parts/parts.csv'
 
-bounding_boxes_csv = pd.read_csv(bounding_boxes_file)
-classes_csv = pd.read_csv(classes_file)
-image_class_labels_csv = pd.read_csv(image_class_labels_file)
-images_csv = pd.read_csv(images_file)
-train_test_split_csv = pd.read_csv(train_test_split_file)
+bounding_boxes = pd.read_csv(bounding_boxes_file)
+classes = pd.read_csv(classes_file)
+image_class_labels = pd.read_csv(image_class_labels_file)
+images = pd.read_csv(images_file)
+train_test_split = pd.read_csv(train_test_split_file)
 
-attributes_csv = pd.read_csv(attributes_file)
-certainties_csv = pd.read_csv(certainties_file)
-class_attribute_labels_continuous_csv = pd.read_csv(class_attribute_labels_continuous_file)
-image_attribute_labels_csv = pd.read_csv(image_attribute_labels_file)
+attributes = pd.read_csv(attributes_file)
+certainties = pd.read_csv(certainties_file)
+class_attribute_labels_continuous = pd.read_csv(class_attribute_labels_continuous_file)
+image_attribute_labels = pd.read_csv(image_attribute_labels_file)
 
-part_click_locs_csv = pd.read_csv(part_click_locs_file)
-part_locs_csv = pd.read_csv(part_locs_file)
-parts_csv = pd.read_csv(parts_file)
+part_click_locs = pd.read_csv(part_click_locs_file)
+part_locs = pd.read_csv(part_locs_file)
+parts = pd.read_csv(parts_file)
